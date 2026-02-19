e918f0891cc5f8bb1c5ddd6593f2420bf9c8fa34
Dur1i1 <borisakmolov@gmail.com>
2026-02-20 00:07:36 +0700

    Add files via upload

---
+++ lab01/SimLab1/SimLab1/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace SimLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.heightBox = new System.Windows.Forms.NumericUpDown();
+            this.heightText = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.angleBox = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.speedBox = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.sizeBox = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.weightBox = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.stepBox = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.angleBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.weightBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stepBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.AxisX.Maximum = 10D;
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisY.Maximum = 25D;
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(64, 138);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(759, 328);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // heightBox
+            // 
+            this.heightBox.Location = new System.Drawing.Point(140, 29);
+            this.heightBox.Name = "heightBox";
+            this.heightBox.Size = new System.Drawing.Size(120, 20);
+            this.heightBox.TabIndex = 1;
+            this.heightBox.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // heightText
+            // 
+            this.heightText.AutoSize = true;
+            this.heightText.Location = new System.Drawing.Point(87, 31);
+            this.heightText.Name = "heightText";
+            this.heightText.Size = new System.Drawing.Size(45, 13);
+            this.heightText.TabIndex = 2;
+            this.heightText.Text = "Высота";
+            this.heightText.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Угол";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // angleBox
+            // 
+            this.angleBox.Location = new System.Drawing.Point(140, 55);
+            this.angleBox.Name = "angleBox";
+            this.angleBox.Size = new System.Drawing.Size(120, 20);
+            this.angleBox.TabIndex = 3;
+            this.angleBox.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Скорость";
+            // 
+            // speedBox
+            // 
+            this.speedBox.Location = new System.Drawing.Point(140, 81);
+            this.speedBox.Name = "speedBox";
+            this.speedBox.Size = new System.Drawing.Size(120, 20);
+            this.speedBox.TabIndex = 5;
+            this.speedBox.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(312, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Размер";
+            // 
+            // sizeBox
+            // 
+            this.sizeBox.Location = new System.Drawing.Point(365, 29);
+            this.sizeBox.Name = "sizeBox";
+            this.sizeBox.Size = new System.Drawing.Size(120, 20);
+            this.sizeBox.TabIndex = 7;
+            this.sizeBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(321, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Вес";
+            // 
+            // weightBox
+            // 
+            this.weightBox.Location = new System.Drawing.Point(365, 57);
+            this.weightBox.Name = "weightBox";
+            this.weightBox.Size = new System.Drawing.Size(120, 20);
+            this.weightBox.TabIndex = 9;
+            this.weightBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.weightBox.ValueChanged += new System.EventHandler(this.weightBox_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(547, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 101);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 472);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(860, 184);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(273, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Начальный шаг";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // stepBox
+            // 
+            this.stepBox.Location = new System.Drawing.Point(365, 83);
+            this.stepBox.Name = "stepBox";
+            this.stepBox.Size = new System.Drawing.Size(120, 20);
+            this.stepBox.TabIndex = 13;
+            this.stepBox.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.stepBox.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 668);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.stepBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.weightBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sizeBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.speedBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.angleBox);
+            this.Controls.Add(this.heightText);
+            this.Controls.Add(this.heightBox);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.angleBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sizeBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.weightBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stepBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown heightBox;
+        private System.Windows.Forms.Label heightText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown angleBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown speedBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown sizeBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown weightBox;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown stepBox;
+    }
+}
+
---
+++ lab01/SimLab1/SimLab1/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Security.Policy;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SimLab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 6;
+            dataGridView1.Columns[0].Name = "параметр dt";
+            dataGridView1.Columns[1].Name = (stepBox.Value).ToString();
+            dataGridView1.Columns[2].Name = (stepBox.Value*0.1M).ToString();
+            dataGridView1.Columns[3].Name = (stepBox.Value*0.01M).ToString();
+            dataGridView1.Columns[4].Name = (stepBox.Value*0.001M).ToString();
+            dataGridView1.Columns[5].Name = (stepBox.Value*0.0001M).ToString();
+
+            dataGridView1.Rows.Add("Дальность полёта, м");
+            dataGridView1.Rows.Add("Максимальная высота, м");
+            dataGridView1.Rows.Add("Скорость в конечной точке, м/с");
+        }
+
+        private void label1_Click(object sender, EventArgs e) { }
+        private void label1_Click_1(object sender, EventArgs e) { }
+        private void weightBox_ValueChanged(object sender, EventArgs e) { }
+
+        const double g = 9.81;
+        const double C = 0.15;
+        const double rho = 1.29;
+
+        double t, x, y, v0, cosa, sina, S, m, k, vx, vy;
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        double dt = 0.1;
+        int curDt = 0;
+
+        double[] ranges = new double[5];
+        double[] maxHeights = new double[5];
+        double[] finalSpeeds = new double[5];
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            double st = (double)stepBox.Value;
+            double[] dts = new double[5] { st, st*0.1, st * 0.01, st * 0.001, st * 0.0001 };
+
+            if (!timer1.Enabled && curDt < dts.Length)
+            {
+                dt = dts[curDt];
+
+                chart1.Series.Add("Траектория-" + dts[curDt].ToString());
+                chart1.Series[chart1.Series.Count - 1].ChartType = SeriesChartType.Line;
+
+                t = 0;
+                x = 0;
+                y = (double)heightBox.Value;
+                v0 = (double)speedBox.Value;
+                double a = (double)angleBox.Value * Math.PI / 180;
+                cosa = Math.Cos(a);
+                sina = Math.Sin(a);
+                S = (double)sizeBox.Value;
+                m = (double)weightBox.Value;
+                k = 0.5 * C * rho * S / Math.Max(m, 1e-7);
+                vx = v0 * cosa;
+                vy = v0 * sina;
+
+                chart1.Series[chart1.Series.Count - 1].Points.AddXY(x, y);
+
+                maxHeights[curDt] = 0;
+
+                curDt++;
+                timer1.Start();
+            }
+
+            if (curDt >= dts.Length && !timer1.Enabled )
+            {
+                timer2.Stop();
+                curDt = 0;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    dataGridView1.Rows[0].Cells[i + 1].Value = ranges[i].ToString("F2");
+                    dataGridView1.Rows[1].Cells[i + 1].Value = maxHeights[i].ToString("F2");
+                    dataGridView1.Rows[2].Cells[i + 1].Value = finalSpeeds[i].ToString("F2");
+                }
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            int loops = (int)(0.033 / dt);
+            if (loops < 1) loops = 1;
+
+            for (int i = 0; i < loops; i++)
+            {
+                t += dt;
+                double v = Math.Sqrt(vx * vx + vy * vy);
+                vx -= k * vx * v * dt;
+                vy -= (g + k * vy * v) * dt;
+                x += vx * dt;
+                y += vy * dt;
+
+                if (y > maxHeights[curDt-1])
+                    maxHeights[curDt-1] = y;
+
+                if (y <= 0)
+                {
+                    ranges[curDt-1] = x;
+                    finalSpeeds[curDt-1] = Math.Sqrt(vx * vx + vy * vy);
+                    timer1.Stop();
+                    break;
+                }
+
+                chart1.Series[chart1.Series.Count - 1].Points.AddXY(x, y);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer2.Enabled) return;
+
+            dataGridView1.Columns[0].Name = "параметр dt";
+            dataGridView1.Columns[1].Name = (stepBox.Value).ToString();
+            dataGridView1.Columns[2].Name = (stepBox.Value * 0.1M).ToString();
+            dataGridView1.Columns[3].Name = (stepBox.Value * 0.01M).ToString();
+            dataGridView1.Columns[4].Name = (stepBox.Value * 0.001M).ToString();
+            dataGridView1.Columns[5].Name = (stepBox.Value * 0.0001M).ToString();
+
+            chart1.Series.Clear();
+            curDt = 0;
+
+            Array.Clear(ranges, 0, ranges.Length);
+            Array.Clear(maxHeights, 0, maxHeights.Length);
+            Array.Clear(finalSpeeds, 0, finalSpeeds.Length);
+
+            timer2.Start();
+        }
+    }
+}
